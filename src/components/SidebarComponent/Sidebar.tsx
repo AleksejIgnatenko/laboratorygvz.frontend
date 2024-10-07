d75659985a13dcb149e71d3a3ee0be0c8ab0c971
Aleksej Ignatenko <ignatenkoaleksej771@gmail.com>
2024-10-07 18:48:01 +0300

    Добавлена боковая панель.

---
+++ src/components/SidebarComponent/Sidebar.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import "./Sidebar.css";
+import "boxicons/css/boxicons.min.css"; // Обновленный импорт стилей Boxicons
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Sidebar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Установите значение в зависимости от состояния входа в систему
+
+  const sidebarItems = [
+    { icon: "bx bx-home-alt-2", name: "Home", tooltip: "Home" },
+    { icon: "bx bx-user", name: "User", tooltip: "User" },
+    { icon: "bx bx-test-tube", name: "Experiments", tooltip: "Experiments" },
+    { icon: "bx bx-folder", name: "File Manager", tooltip: "Files" },
+    { icon: "bx bx-cart-alt", name: "Order", tooltip: "Order" },
+    { icon: "bx bx-heart", name: "Saved", tooltip: "Saved" },
+    { icon: "bx bx-cog", name: "Setting", tooltip: "Setting" },
+  ];
+
+  useEffect(() => {
+    const sidebar = document.querySelector(".sidebar");
+    const closeBtn = document.querySelector("#btn");
+    const searchBtn = document.querySelector(".bx-search");
+
+    const menuBtnChange = () => {
+      if (sidebar) {
+        if (sidebar.classList.contains("open")) {
+          closeBtn?.classList.replace("bx-menu", "bx-menu-alt-right");
+        } else {
+          closeBtn?.classList.replace("bx-menu-alt-right", "bx-menu");
+        }
+      }
+    };
+
+    const handleSidebarToggle = () => {
+      if (sidebar) {
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+      }
+    };
+
+    closeBtn?.addEventListener("click", handleSidebarToggle);
+    searchBtn?.addEventListener("click", handleSidebarToggle);
+
+    return () => {
+      closeBtn?.removeEventListener("click", handleSidebarToggle);
+      searchBtn?.removeEventListener("click", handleSidebarToggle);
+    };
+  }, []);
+
+  return (
+    <div className="sidebar">
+      <div className="logo-details">
+        <i className="bx bxl-codepen icon"></i>
+        <div className="logo_name">SideMenu</div>
+        <i className="bx bx-menu" id="btn"></i>
+      </div>
+      <ul className="nav-list">
+        <li>
+          <i className="bx bx-search"></i>
+          <input type="text" placeholder="Search..." />
+          <span className="tooltip">Search</span>
+        </li>
+        {sidebarItems.map((item, index) => (
+          <li key={index}>
+            <Link href="#">
+              <i className={item.icon}></i>
+              <span className="links_name">{item.name}</span>
+            </Link>
+            <span className="tooltip">{item.tooltip}</span>
+          </li>
+        ))}
+        {isLoggedIn ? (
+          <li className="profile">
+            <div className="profile-details">
+              <i className="bx bx-id-card"></i>
+              <div className="name_job">
+                <div className="name">Account</div>
+              </div>
+            </div>
+            <i className="bx bx-user" id="log_in"></i>
+          </li>
+        ) : (
+          <li className="profile">
+            <div className="profile-details">
+              <i className="bx bx-export"></i>
+              <div className="name_job">
+                <div className="name">Login</div>
+              </div>
+            </div>
+            <i className="bx bx-log-in" id="log_in"></i>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+}
